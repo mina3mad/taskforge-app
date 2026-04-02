a73dae8410a5a3a6fb80ac07c8b96f5a42aad330
mina emad <minaemad240@gmail.com>
2026-04-03 00:08:46 +0200

    add service and controller in users module

---
+++ apps/api/src/app/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { RefreshToken } from '../auth/tokens/entities/refresh-token.entity';
+import { UserResponseDto } from './dto/user-response.dto';
+import { plainToInstance } from 'class-transformer';
+import { Response } from 'express';
+
+@Injectable()
+export class UsersService {
+    constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(RefreshToken)
+    private readonly refreshTokenRepository: Repository<RefreshToken>,
+  ) {}
+
+  async findOne(id: string): Promise<UserResponseDto> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException("User not found");
+    }
+    return plainToInstance(UserResponseDto, user, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  async logout(userId: string, res: Response) {
+    await this.refreshTokenRepository.delete({
+      user: { id: userId },
+    });
+
+    res.clearCookie('refreshToken', {
+      path: '/api/token/refresh',
+    });
+
+    return { message:"Logout successfully"};
+  }
+}
